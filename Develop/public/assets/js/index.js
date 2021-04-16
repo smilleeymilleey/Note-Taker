519b258306df64c350a87420b5d0ba430320a511
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-15 19:20:22 -0500

    added a server.js to add express code to

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,11 +1,4 @@
-const express = require("express");
-const path = require("path");
 
-const app = express();
-const port = process.env.PORT || "8000";
-
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
 
 let noteTitle;
 let noteText;
@@ -188,12 +181,3 @@ if (window.location.pathname === '/notes') {
 
 getAndRenderNotes();
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get("/notes" (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
-
-
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-  });
---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+
+
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
