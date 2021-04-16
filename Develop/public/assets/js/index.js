dd66e3ad60f086b64de5fe195835c4d2956cab5e
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-15 19:10:22 -0500

    added nodemon and specified paths for the URL

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -178,3 +187,13 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get("/notes" (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
