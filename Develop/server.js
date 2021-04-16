6f039ec001a52e8d1add8bc3f2943490876738f2
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-15 19:23:48 -0500

    added functionality to serve both html pages

--- Develop/server.js
+++ Develop/server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 
 app.listen(port, () => {
