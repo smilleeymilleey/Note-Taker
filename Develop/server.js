8d6b97cb48fc195592c306a9767c6f48961012a5
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-15 20:06:40 -0500

    added notes from write file to be saved to database

--- Develop/server.js
+++ Develop/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-
+app.use(express.static('public'))
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
@@ -23,7 +23,11 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.post('/api/notes', (req, res) => {
-    let newNote = fs.readFileSync()
+    let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    notes.push(req.body)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  
+    res.json(notes);
 
 
 })
