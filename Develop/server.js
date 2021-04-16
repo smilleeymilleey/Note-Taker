a8c40bef4b6ecb2e6e2ca29d62596949fbf4b74e
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-15 19:40:46 -0500

    read from database

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { rsort } = require("semver");
 
 const app = express();
 const port = process.env.PORT || "8000";
@@ -13,6 +15,18 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    let notes = fs.readFileSync('./db/db.json');
+    let notesJson = JSON.parse(notes);
+    res.json(notesJson)
+})
+
+app.post('/api/notes', (req, res) => {
+    let newNote = fs.readFileSync()
+
+
+})
 
 
 app.listen(port, () => {
